4c23738743767000817f01c7eae275f8e5cf6192
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2024-10-06 15:28:09 +0400

    add initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h3>App</h3>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
